73cc2a25c289a43e7cfb826cf4658318021cc47c
Charles Strahan <charles.c.strahan@gmail.com>
2014-06-23 04:00:28 -0400

    remove old files

--- temp.nix
+++
@@ -1,8 +0,0 @@
-
-    (import <nixpkgs> { }).fetchFromGitHub {
-      owner = "mitchellh";
-      repo = "cli";
-      rev = "df3e8ad8d1c";
-      sha256 = "0000000000000000000000000000000000000000000000000000";
-    }
-  
