d52214248426f47c410d284e8c76b33aff29fcce
Charles Strahan <charles.c.strahan@gmail.com>
2014-09-23 22:14:32 -0400

    add default.nix

---
+++ default.nix
@@ -0,0 +1,30 @@
+with import <nixpkgs> {};
+
+stdenv.mkDerivation {
+  name = "go2nix";
+
+  src = ./.;
+
+  buildInputs = with rubyLibs; [
+    ruby yajl_ruby erubis
+    go go-repo-root
+    git mercurial bazaar subversion
+  ];
+
+  installPhase = ''
+    cp -r $src $out
+    chmod -R +w $out
+
+    mv $out/bin/{,.}go2nix
+    cat <<EOF > $out/bin/go2nix
+#!/bin/sh
+export PATH=${lib.makeSearchPath "bin" [ ruby go go-repo-root git mercurial bazaar subversion ]}:$PATH
+export GIT_SSL_CAINFO="${cacert}/etc/ca-bundle.crt"
+export RUBYOPT=rubygems
+export GEM_PATH=${lib.makeSearchPath ruby.gemPath (with rubyLibs; [ yajl_ruby erubis ])}
+
+ruby $out/bin/.go2nix "\$@"
+EOF
+    chmod +x $out/bin/go2nix
+  '';
+}
